e21912cb46c804f99e2d685b7e05354e06277597
Nguyen Loan <Admin@LAPTOP-Q62KFUJF>
2023-12-04 17:17:51 +0700

    Web thue tro

---
+++ about.php
@@ -0,0 +1,97 @@
+<?php
+ include './header.php';
+?>
+<div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                        <div class="about-img position-relative overflow-hidden p-5 pe-0">
+                            <img class="img-fluid w-100" src="img/about.jpg">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                        <h1 class="mb-4">Tại sao lại chọn PhongTro2002?</h1>
+                        <p class="mb-4">Chúng tôi biết bạn có rất nhiều lựa chọn, nhưng Phongtro2002 tự hào là trang web đứng top google về các từ khóa: cho thuê phòng trọ, nhà trọ, thuê nhà nguyên căn, cho thuê căn hộ, tìm người ở ghép, cho thuê mặt bằng...Vì vậy tin của bạn đăng trên website sẽ tiếp cận được với nhiều khách hàng hơn, do đó giao dịch nhanh hơn, tiết kiệm chi phí hơn</p>
+                        <p><i class="fa fa-check text-primary me-3"></i><span class="whyus-countup-item-number">116.998+</span><span class="whyus-countup-item-text">Thành viên</span></p>
+                        <p><i class="fa fa-check text-primary me-3"></i><span class="whyus-countup-item-number">103.348+</span><span class="whyus-countup-item-text">Tin đăng</span></p>
+                        <p><i class="fa fa-check text-primary me-3"></i><span class="whyus-countup-item-number">300.000+</span><span class="whyus-countup-item-text">Lượt truy cập/tháng</span></p>
+                        <p><i class="fa fa-check text-primary me-3"></i>Chi phí thấp, hiệu quả tối đa</p>
+                        <p class="testimonial">"Trước khi biết website phongtro2002, mình phải tốn nhiều công sức và chi phí cho việc đăng tin cho thuê: từ việc phát tờ rơi, dán giấy, và đăng lên các website khác nhưng hiệu quả không cao. Từ khi biết website phongtro123.com, mình đã thử đăng tin lên và đánh giá hiệu quả khá cao trong khi chi phí khá thấp, không còn tình trạng phòng trống kéo dài." <br><span style="font-style: normal; display: block; margin-top: 10px;">Anh Khánh (chủ hệ thống phòng trọ tại Tp.HCM)</span></p>
+                        <h6 class="section-title">Bạn đang có phòng trọ / căn hộ cho thuê?</h6>
+                        <p>Không phải lo tìm người cho thuê, phòng trống kéo dài</p>
+                        <a class="btn red" style="padding: 10px 30px;" rel="nofollow" href="">Đăng tin ngay</a></div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+
+
+        <!-- Call to Action Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="bg-light rounded p-3">
+                    <div class="bg-white rounded p-4" style="border: 1px dashed rgba(0, 185, 142, .3)">
+                        <div class="row g-5 align-items-center">
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <img class="img-fluid rounded w-100" src="/public/header/img/call-to-action.jpg" alt="">
+                            </div>
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                                <div class="mb-4">
+                                    <h1 class="mb-3">Liên hệ với chúng tôi nếu bạn cần</h1>
+                                 
+                                </div>
+                                <div class="support-item"><span class="support-item-title">Hỗ trợ đăng tin</span><a rel="nofollow" href="tel:0901424123">Điện thoại: 0964557349</a><a rel="nofollow" target="_blank" href="https://zalo.me/0901424123">Zalo: 0964557349</a></div>
+                                <div class="support-item"><span class="support-item-title">Phản ánh/Khiếu nại</span><a rel="nofollow" href="tel:0901424123">Điện thoại: 0964557349</a><a rel="nofollow" target="_blank" href="https://zalo.me/0901424123">Zalo: 0964557349</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Call to Action End -->
+
+
+        <!-- Team Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h1 class="mb-3">About</h1>
+                </div>
+                <div class="row g-4">
+                    <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="team-item rounded overflow-hidden">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/Loan.jpg" alt="">
+                                <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                            <div class="text-center p-4 mt-3">
+                                <h5 class="fw-bold mb-0">Full Name</h5>
+                                <small>Nguyễn Thị Loan</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="team-item rounded overflow-hidden">
+                            <div class="position-relative">
+                                <img class="img-fluid" src="img/K.jpg" alt="">
+                                <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                            <div class="text-center p-4 mt-3">
+                                <h5 class="fw-bold mb-0">Full Name</h5>
+                                <small>Phan Thị Kiên</small>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </div>
